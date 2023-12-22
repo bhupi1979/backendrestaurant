cbe82d3d66fce674837864c5cd002f57167fb7a4
bhupi1979 <bhupendrapaliwal1983@gmail.com>
2023-12-22 17:04:27 +0530

    demochange

---
+++ index.js
@@ -0,0 +1,30 @@
+
+const express = require('express')
+require('./db/config.js')
+const cors = require('cors')
+const app = express()
+const mongoose = require('mongoose')
+const user = require('./db/user')
+
+app.use(express.json()) // Make sure this middleware is applied before your route
+app.use(cors())
+//api for registration
+app.post('/register',async(req,res)=>{
+      let user1=new user(req.body)
+      console.log('the result is'+req.body.password)
+      let result= await user1.save()
+      res.send(result)
+      //console.warn(result)
+})
+//api for login
+app.post('/login',async(req,res)=>{
+      let user1= await user.findOne(req.body).select("-password")
+      console.log('the result is'+user1)
+      //let result= await user1.find(user1.name)
+      if(user1)
+      res.send({resultuser:1})
+else
+res.send({resultuser:0})
+      //console.warn(result)
+})
+app.listen(5000)
