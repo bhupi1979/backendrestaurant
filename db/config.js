c1992f0ae38ef99530918126befb44ec2b0a1b11
bhupi1979 <bhupendrapaliwal1983@gmail.com>
2023-12-24 20:10:56 +0530

    with dataabase modify

--- db/config.js
+++ db/config.js
@@ -1,2 +1,29 @@
 const mongoose=require('mongoose')
- mongoose.connect('mongodb+srv://bhupi:bhupi@123@cluster0.td911ok.mongodb.net/ecom-123?retryWrites=true&w=majority')
+//  mongoose.connect('mongodb+srv://bhupi:bhupi@123@cluster0.ey8speb.mongodb.net/ecom-123?retryWrites=true&w=majority'
+// //  ,{
+// // useNewUrlParser:true,
+// // useCreateIndex:true,
+// // useUnifiedTopology:true,
+// // useFindAndModify:false
+// // }
+// ).then(()=>{
+//     console.log('connection sucessful')
+//  }).catch((err)=>{console.log(err)
+
+//  })
+const username = 'bhupi';
+const password = 'bhupi@123';
+const database = 'ecom-123';
+
+const uri = `mongodb+srv://${encodeURIComponent(username)}:${encodeURIComponent(password)}@cluster0.ey8speb.mongodb.net/${database}?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+ 
+  useUnifiedTopology: true
+ 
+}).then(() => {
+  console.log('Connection successful');
+}).catch((err) => {
+  console.log(err);
+});
