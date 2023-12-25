d191fb1894d3d5bcb4eb4dae461a7cba5e18bff7
bhupi1979 <bhupendrapaliwal1983@gmail.com>
2023-12-25 12:48:54 +0530

    corschanges

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const user = require('./db/user')
 
 app.use(express.json()) // Make sure this middleware is applied before your route
 app.use(cors())
+app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', 'https://cool-choux-0cdf69.netlify.app');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type');
+      next();
+    });
 //api for registration
 app.post('/register',async(req,res)=>{
       let user1=new user(req.body)
