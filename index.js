73764f7bb9efb75d9dcaf119eaeaf93e7a52985e
bhupi1979 <bhupendrapaliwal1983@gmail.com>
2023-12-26 19:48:59 +0530

    ffffff

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ app.get("/singleproductcategory/:id",async (req,res)=>{
 const multer  = require('multer')
 const storage = multer.diskStorage({
       destination: function (req, file, cb) {
-        cb(null, 'uploads')
+        cb(null, 'uploads/')
       },
       filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() 
@@ -125,9 +125,7 @@ const storage = multer.diskStorage({
 app.post('/productdetail',upload.single('image'),async(req,res)=>{
       const { name, price,descp,pcategory } = req.body;
       let image=req.file.filename;
-      let pdetail=new productdetail({
-            name,price,descp,pcategory,image
-      })
+      let pdetail=new productdetail({name,price,descp,pcategory,image})
       let result= await pdetail.save()
      res.send(result)
 })
