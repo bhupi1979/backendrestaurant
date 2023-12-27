a4960088e40086e3d744662fbb4e687c5b23c693
bhupi1979 <bhupendrapaliwal1983@gmail.com>
2023-12-27 19:40:08 +0530

    rrrrrrrrrrrr

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ const productdetail = require('./db/productdetail.js')
 app.use(express.json()) // Make sure this middleware is applied before your route
 app.use(cors())
 app.use('/uploads', express.static('uploads'));
-app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', 'https://cool-choux-0cdf69.netlify.app');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      res.header('Access-Control-Allow-Headers', 'Content-Type');
-      next();
-    });
+// app.use((req, res, next) => {
+//       res.header('Access-Control-Allow-Origin', 'https://cool-choux-0cdf69.netlify.app');
+//       res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//       res.header('Access-Control-Allow-Headers', 'Content-Type');
+//       next();
+//     });
 //api for registration
 app.post('/register',async(req,res)=>{
       let user1=new user(req.body)
